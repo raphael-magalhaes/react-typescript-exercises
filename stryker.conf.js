90e2da3fb866c31af630143af0a41f17cb2d6af6
Raphael Magalhães <raphael.sdm@gmail.com>
2020-04-10 20:02:27 -0300

    Make Stryker ignore files created by create-react-app

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,7 +4,12 @@
 module.exports = {
     comment:
         'This config was generated using a preset. Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react',
-    mutate: ['src/**/*.ts?(x)', '!src/**/*@(.test|.spec|Spec).ts?(x)'],
+    mutate: [
+        'src/**/*.ts?(x)',
+        '!src/**/*@(.test|.spec|Spec).ts?(x)',
+        '!src/index.tsx',
+        '!src/serviceWorker.ts'
+    ],
     mutator: 'typescript',
     testRunner: 'jest',
     reporters: ['progress', 'clear-text', 'html'],
