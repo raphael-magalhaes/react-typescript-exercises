664b165da59c274dfa7e423d47e64dd0723c100c
Raphael Magalhães <raphael.sdm@gmail.com>
2020-04-13 11:32:01 -0300

    [TSX-0002][TSX-0003] Render DogDetails in App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import App from './App'
 import { Notification } from './Common/Notification'
+import App, { testables } from './App'
+
+jest.mock('./Components/DogDetails', () => {
+    return {
+        DogDetails: ({ onBark }: { onBark: () => {} }) => {
+            onBark()
+            return <div data-testid="app__dog-details"></div>
+        }
+    }
+})
 
 Notification.alert = jest.fn()
 
@@ -25,5 +34,37 @@ describe('App', () => {
 
         // Then
         expect(Notification.alert).toHaveBeenCalledWith('Howdy, stranger.')
+    })
+
+    it('doBark() should call Notification.alert() with the expected message', () => {
+        // When
+        testables.doBark()
+
+        // Then
+        expect(Notification.alert).toHaveBeenCalledWith('Woof! Woof!')
+    })
+
+    it('should have <DogDetails/>', () => {
+        // Given
+        const { getByTestId } = render(<App />)
+        const dogDetailsElement = getByTestId('app__dog-details')
+
+        // Then
+        expect(dogDetailsElement).toBeInTheDocument()
+    })
+
+    // FIXME: The problem is that DogDetails.doBark (not exported) and
+    // testables.doBark (exported) are not the same reference. Please
+    // note that DogDetails is mocked to receive and call onBark prop
+    // before returning.
+    // If testing-library/react is not able to achieve a test like this
+    // then try using Enzyme (see https://enzymejs.github.io/enzyme/).
+    it.skip('should pass the doBark() to <DogDetails/> onBark prop', () => {
+        // Given
+        jest.spyOn(testables, 'doBark')
+        render(<App />)
+
+        // Then
+        expect(testables.doBark).toHaveBeenCalled()
     })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import logo from './logo.svg'
 import { Notification } from './Common/Notification/Notification'
 import './App.css'
+import { DogDetails } from './Components/DogDetails'
 
 const emitAlert = () => Notification.alert('Howdy, stranger.')
+const doBark = () => Notification.alert('Woof! Woof!')
 
 function App() {
     return (
@@ -24,9 +26,19 @@ function App() {
                 <button data-testid="app__emit-alert" onClick={emitAlert}>
                     Emit Alert
                 </button>
+                <DogDetails
+                    testId="app__dog-details"
+                    title="Dog"
+                    imageURL="https://via.placeholder.com/200x200?text=Dog%20Image"
+                    onBark={doBark}
+                />
             </header>
         </div>
     )
 }
 
 export default App
+
+export const testables = {
+    doBark
+}
